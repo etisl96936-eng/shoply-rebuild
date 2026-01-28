4da462f73cba5ad79841e7a5171db7bafefd5c3c
Eti Levi <etisl96936@gmail.com>
2026-01-28 23:16:27 +0200

    Fix network package structure and DTO organization

--- shoply_app/app/build.gradle.kts
+++ shoply_app/app/build.gradle.kts
@@ -51,6 +51,9 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation("androidx.compose.material:material-icons-extended")
     implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
 
 
     // ניווט
